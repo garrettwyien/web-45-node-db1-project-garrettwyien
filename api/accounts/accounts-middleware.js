eb8555552b50810765ec663b4ded6d700aad0023
Garrett Yien <yien.garrett94@gmail.com>
2021-09-07 16:49:25 -0400

    added checkaccountpayload middleware function

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,18 @@
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  if(!req.body.name || !req.body.budget) {
+    next({status:400,message:"name and budget are required"})
+  }else if(typeof req.body.name !== 'string') {
+    next({status:400,message:"name of account must be a string"})
+  }else if(req.body.name.length < 3 || req.body.name.length > 100) {
+    next({status:400,message:"name of account must be between 3 and 100"})
+  }else if(typeof req.body.budget !== 'number'){
+    next({status:400,message:"budget of account must be a number"})
+  }else if(req.body.budget < 0 || req.body.budget > 1000000){
+    next({status:400,message:"budget of account is too large or too small"})
+  } else {
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
